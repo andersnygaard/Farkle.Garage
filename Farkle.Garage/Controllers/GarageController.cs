88d5d63f9148b4fc7a4ee82f2149c3aa08302feb
andersnygaard <anders.nygaard@gmail.com>
2018-02-14 10:59:28 +0100

    Initial commit

---
+++ Farkle.Garage/Controllers/GarageController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farkle.Garage.Controllers
+{
+    [Route("api/[controller]")]
+    public class GarageController : Controller
+    {
+        // GET api/garage
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        // POST api/garage
+        [HttpPost]
+        public void Post([FromBody]string title)
+        {
+
+        }
+    }
+}
---
+++ Farkle.Garage/Models/Garage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Farkle.Garage.Models
+{
+    public class Garage
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
